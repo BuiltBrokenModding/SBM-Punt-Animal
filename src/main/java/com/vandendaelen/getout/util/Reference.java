5a3fb85ad31de5ee92b95ffcea592d3481ab9bb6
LotuxPunk <clement@vandendaelen.com>
2019-01-30 00:27:01 +0100

    Initial commit

---
+++ src/main/java/com/vandendaelen/getout/util/Reference.java
@@ -0,0 +1,15 @@
+package com.vandendaelen.getout.util;
+
+public class Reference {
+    public static final String MODID = "getout";
+    public static final String MOD_NAME = "GetOut";
+
+    public static class VERSION{
+        public static final String MCVERSION = "1.12.2";
+        public static final String MAJORMOD = "0";
+        public static final String MAJORAPI = "0";
+        public static final String MINOR = "0";
+        public static final String PATCH = "1";
+        public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
+    }
+}
