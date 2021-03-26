78ac514d0b725b1f375e0b24a2b189ae77f3ddfb
Robin Seifert <rseifert.phone@gmail.com>
2021-03-26 10:13:53 -0400

    Updated to 1.16.5

--- src/main/java/com/builtbroken/puntanimal/PuntAnimal.java
+++ src/main/java/com/builtbroken/puntanimal/PuntAnimal.java
@@ -1,45 +1,40 @@
 package com.builtbroken.puntanimal;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod("sbmpuntanimal")
 @Mod.EventBusSubscriber
 public class PuntAnimal
 {
     public static final Logger LOGGER = LogManager.getLogger();
+
     @SubscribeEvent
     public static void onDamage(LivingAttackEvent event)
     {
         try
         {
-            final Entity source = event.getSource().getTrueSource();
+            final Entity source = event.getSource().getDirectEntity();
             if (source instanceof PlayerEntity)
             {
                 PlayerEntity attacker = (PlayerEntity) source;
                 LivingEntity victim = event.getEntityLiving();
 
-                if (attacker.isSneaking() && victim instanceof AnimalEntity) //TODO set allow/block list for victim
+                if (attacker.isCrouching() && victim instanceof AnimalEntity) //TODO set allow/block list for victim
                 {
                     event.setCanceled(true);
 
-                    int xr = (int) -(victim.getPosX() - attacker.getPosX());
-                    int zr = (int) -(victim.getPosZ() - attacker.getPosZ());
-
-                    //When xr & zr == 0, both are on the same block, we don't know where to push
-                    if (xr != 0 || zr != 0)
-                    {
-                        victim.knockBack(attacker, 0.5F, xr, zr);
-                    }
-                    //TODO push randomly into nearest free spot for else
+                    victim.knockback(0.5F,
+                            MathHelper.sin(attacker.yRot * ((float)Math.PI / 180F)),
+                            -MathHelper.cos(attacker.yRot * ((float)Math.PI / 180F)));
                 }
             }
         }
